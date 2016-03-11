87d57f4480785e7c55b9dc5f659e755c0a0de4f6
moonlight83340 <moonlight83340@users.noreply.github.com>
2016-03-11 21:33:21 +0100

    delete jumbontron

--- App/Views/layout.html.php
+++ App/Views/layout.html.php
@@ -21,7 +21,7 @@
         <![endif]-->
     </head>
     <body>
-        <nav class="navbar navbar-inverse navbar-fixed-top">
+        <nav id="navbar-pokedex" class="navbar navbar-inverse navbar-fixed-top">
             <div class="container">
               <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -30,17 +30,17 @@
                   <span class="icon-bar"></span>
                   <span class="icon-bar"></span>
                 </button>
-                  <a class="navbar-brand" href="<?= \PoireauFramework\Helper\Url::base()?>">Pokiut Api Pokedex</a>
+                  <a id="navbar-brand-pokedex" class="navbar-brand" href="<?= \PoireauFramework\Helper\Url::base()?>">Pokiut Api Pokedex</a>
               </div>
               <div id="navbar" class="navbar-collapse collapse">
                 <form class="navbar-form navbar-right">
                     <div class="form-group">
-                      <input placeholder="Email" class="form-control" type="text">
+                      <input placeholder="pseudo" class="form-control" type="text">
                     </div>
                     <div class="form-group">
                       <input placeholder="Password" class="form-control" type="password">
                     </div>
-                    <button type="submit" class="btn btn-success">Sign in</button>
+                    <button type="submit" class="btn btn-success">Connexion</button>
                 </form>
               </div><!--/.navbar-collapse -->
             </div>
